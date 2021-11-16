f9225334e7d2fdebead450bf7ee4df2930561a29
Ben Wilson <benjaminw1030@gmail.com>
2021-11-16 08:50:52 -0800

    update from lesson

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Header from "./Header";
+import TicketControl from "./TicketControl";
 import TicketList from "./TicketList";
 
 function App(){
   return(
     <React.Fragment>
         <Header />
-        <TicketList />
+        <TicketControl />
     </React.Fragment>
   );
 }
